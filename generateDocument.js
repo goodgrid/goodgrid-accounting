f59802ec44c6bda4a539e0fd22f7208f2b46c483
Koen Bonnet <koen@goodgrid.nl>
2023-05-05 13:55:25 +0200

    Minor layout changes

--- generateDocument.js
+++ generateDocument.js
@@ -68,7 +68,7 @@ const generateInvoice = async (invoice) => {
             }, {
                 property: "grandtotal",
                 label: "Totaal incl. BTW",
-                width: 80,
+                width: 150,
                 headerColor: "white",
                 renderer: (value, indexColumn, indexRow, row, rectRow, rectCell) => getCurrency(value)
             }
@@ -99,8 +99,8 @@ const generateInvoice = async (invoice) => {
     })
 
     await doc.table(summaryTable,{
-        width: 200,
-        x: 330,
+        width: 350,
+        x: 300,
         padding: 5,
         prepareHeader: () => doc.font(`assets/${basefont}-Bold.ttf`).fontSize(6),
         prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
@@ -189,9 +189,11 @@ const generateTimesheet = async (invoice) => {
     doc.image('assets/Goodgrid_logo_CMYK_horizontal_right_blue.png',380, 40, {width: 180})
         
     doc.font(`assets/${basefont}-Thin.ttf`).fontSize("10").fillColor("#36599B")
-    doc.text(`${customerName}\r${customerAddress}`,50,150)
-    doc.text(`Periode: ${period}`,380,150)
+    doc.text(`${customerName}`,50,150)
 
+    doc.text(`Behorend bij factuur ${invoiceNumber}`,300,150)
+    doc.text(`Periode: ${period}`,300,165)
+    
 
     await doc.table(timesheetTable,{
         width: 800,
